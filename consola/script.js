007374d483c7f56acafaf578526bc06d2d8c517e
JORGEDIPRA\JorgeEdwin <jorgedipra@gmail.com>
2019-04-30 15:56:39 -0500

    se agregan interacciones de manera estática

--- consola/script.js
+++ consola/script.js
@@ -1,61 +1,72 @@
 const app = new Vue({
     el: '#app',
     a: false,
-    data:{ 
-      mensaje: [],
-      actividad: '',
-      actividades: [],
-      consola: [],
-      messages: []
+    data: {
+        mensaje: [],
+        actividad: '',
+        actividades: [],
+        consola: [],
+        messages: []
     },
-    mounted(){
-        dataconsolaInicio ={
+    mounted() {
+        dataconsolaInicio = {
             consola: "- Console Asisitent"
         }
         this.mensaje.push(dataconsolaInicio);
     },
-    updated(){             
+    updated() {
         var messageDisplay = app.$refs.messageDisplay;
         messageDisplay.scrollTop = messageDisplay.scrollHeight;
     },
     methods: {
-      actualizarLista: function() {
-        User="User"
-        if(localStorage.getItem("user"))
-            User= localStorage.getItem("user");
-        
-        data = {
-            user: User, 
-            message: this.actividad,
-            rol: "User"
-        };
-        
-        switch (this.actividad) {
-            case "hola":
-                hola();
-                break;
-            case "adios":
-                adios();
-                break;
-            default:
-                break;
+        actualizarChat: function () {
+            User = "Us"
+            if (localStorage.getItem("user"))
+                User = localStorage.getItem("user");
+
+            data = {
+                user: User,
+                message: this.actividad,
+                rol: "User"
+            };
+
+            const know = {
+                "hola!"       : "hola: D",
+                "adios"      : "exit",
+                "como estas?": "bien y tu?",
+                "salir"      : "adios!"
+            };
+            if (this.actividad in know) {
+                message(know[this.actividad]);
+            } else {
+                message("No te entiendo, estas Jugando?");
+            }
+            switch (this.actividad) {
+                case "hola":
+                    hola();
+                    break;
+                case "adios":
+                    adios();
+                    break;
+                default:
+                    break;
+            }
+            setTimeout(() => {
+                this.actividades.push(data);
+            }, 500);
+            this.actividad = null;
         }
-        setTimeout(() => {
-            this.actividades.push(data);
-        }, 500);
-        this.actividad = null;
-      }
     }
 })
 
 
-const hola =()=>{
-    if(localStorage.getItem("user")){
+const hola = () => {
+    if (localStorage.getItem("user")) {
         console.log("ok");
-        
+
         data2 = {
-            user:"Alis", 
-            message: "Dime "+localStorage.getItem("user")+", en que te puedo ayudar?",
+            user: "Al",
+            message: "Dime " + localStorage.getItem("user") + ", en que te puedo ayudar?",
             rol: "Alis"
         };
         setTimeout(() => {
@@ -63,21 +74,21 @@ const hola =()=>{
         }, 1000);
         return false;
     }
-    dataconsola ={
+    dataconsola = {
         consola: "XD"
     }
     app.mensaje.push(dataconsola);
     newuser = window.prompt('hola, como te llamas?')
     localStorage.setItem("user", newuser);
     data2 = {
-        user:"Alis", 
-        message: "hola,"+newuser,
+        user: "Al",
+        message: "hola," + newuser,
         rol: "Alis"
     };
     setTimeout(() => {
         app.actividades.push(data2);
         data2 = {
-            user:"Alis", 
+            user: "Al",
             message: "En que te puedo ayudar",
             rol: "Alis"
         };
@@ -85,19 +96,31 @@ const hola =()=>{
             app.actividades.push(data2);
         }, 500);
     }, 1000);
-    
+
 }
-const adios =()=>{
+const adios = () => {
 
     data2 = {
-        user    :"Alis",
-        message : "Adios,"+localStorage.getItem("user"),
-        rol     : "Alis"
+        user: "Al",
+        message: "Adios," + localStorage.getItem("user"),
+        rol: "Alis"
     };
     localStorage.removeItem("user");
     localStorage.clear();
     setTimeout(() => {
         app.actividades.push(data2);
     }, 1000);
-    
+
+}
+
+
+const message = (msg = "") => {
+    data2 = {
+        user: "Al",
+        message: msg,
+        rol: "Alis"
+    };
+    setTimeout(() => {
+        app.actividades.push(data2);
+    }, 1000);
 }
