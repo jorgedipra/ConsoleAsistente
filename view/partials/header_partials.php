794f171c69361b4edbdb1cc9987130403e4c98c5
JORGEDIPRA\JorgeEdwin <jorgedipra@gmail.com>
2019-05-14 15:43:42 -0500

    .ico en meta

--- view/partials/header_partials.php
+++ view/partials/header_partials.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title><?=Titulo?></title>
 	<link rel="shortcut icon" href="storage/logo/ico.ico" />
-	<link rel="apple-touch-icon" href="storage/logo/ico.jpg" />
+	<link rel="apple-touch-icon" href="storage/logo/ico.ico" />
 	<link rel="stylesheet" href="<?=url('public/css/'.$match["name"].'.css')?>">
 	<link rel="stylesheet" href="public/files/bootstrap-4.0.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="public/css/font/awesome5/css/fontawesome-all.min.css">
