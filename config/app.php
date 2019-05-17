1053e158c0b4d3ee4ce49b7560421b376ba1f7e7
JORGEDIPRA\JorgeEdwin <jorgedipra@gmail.com>
2019-05-16 19:43:57 -0500

    expirtar cache

--- config/app.php
+++ config/app.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-header("Expires: Tue, 03 Jul 2030 06:00:00 GMT");
+header("Expires: Tue, 03 Jul 2000 06:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
