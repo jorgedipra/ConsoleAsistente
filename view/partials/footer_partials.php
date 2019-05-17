7654fe0c5069b266c3a5d8edf722170659b29a25
JORGEDIPRA\JorgeEdwin <jorgedipra@gmail.com>
2019-05-16 19:46:39 -0500

    cahe

--- view/partials/footer_partials.php
+++ view/partials/footer_partials.php
@@ -2,7 +2,7 @@
 <script src="public/files/vue/<?=$js_vue?>"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>	
 <script src="public/js/<?=$js_general?>"></script>
-<script src="public/js/<?=$match["name"]?>.js?v1=1"></script>
+<script src="public/js/<?=$match["name"]?>.js?v=1.0"></script>
 <script src="public/js/<?=$match["name"]?>/inputs.js"></script>
 <script src="public/js/<?=$match["name"]?>/neuronas.js"></script>
 <script src="public/js/<?=$match["name"]?>/output.js"></script>	
