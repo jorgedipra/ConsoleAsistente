ca0e6c22cdbd5a46c423bedf837dae375f7a220b
Jorge Diaz <jorgedipra@gmail.com>
2019-12-05 12:35:30 -0500

    notas ejemplos de como se usa

--- public/js/home/dataStack.js
+++ public/js/home/dataStack.js
@@ -1,8 +1,27 @@
 /**
- * JS Data Structures
- * https://github.com/Xabadu/js-data-structures
- *
+* @name    : JS Data Structures
+* @author  : Xabadu
+* @license : open 
+* @repository: https://github.com/Xabadu/js-data-structures
+* @version 1 on 19 Apr 2018
+* @description JS Data Structures
 */
+/**
+ * @version 1.1 on 03 Dic 2019
+ * @author @jorgedipra
+ * @constructor
+ *   @ this.stack = {};
+ *   @ this.count = 0;
+ * @Methods:
+ *   @ push  :  agrega nuevo elemento
+ *   @ pop   :  desocupa un elemnto
+ *   @ peek  :  muestra el ultimo elemento
+ *   @ size  :  cantidad de elementos
+ *   @ cero  :  elimina el objeto
+ *   @ all   :  retorna todos los element
+ *   @ one   :  retorna elemento selecionado
+ *   @ print :  imprime por consola
+ */
 class Stack {
     constructor() {
       this.stack = {};
@@ -45,15 +64,20 @@ class Stack {
     }
   }
   const stack = new Stack();
-// console.log(stack.size()); // 0
-// console.log(stack.push('John Cena')); // { '0': 'John Cena' }
-// console.log(stack.size()); // 1
-// console.log(stack.peek()); // John Cena
-// console.log(stack.push('The Rock')); // { '0': 'John Cena', '1': 'The Rock' }
-// console.log(stack.size()); // 2
-// stack.print(); // { '0': 'John Cena', '1': 'The Rock' }
-// console.log(stack.peek()); // The Rock
-// console.log(stack.pop()); // The Rock
-// stack.print(); // { '0': 'John Cena' }
-// console.log(stack.size()); // 1
-// console.log(stack.peek()); // John Cena
+/**
+  @example #NOTE
+ * 
+console.log(stack.size()); // 0
+console.log(stack.push('John Cena')); // { '0': 'John Cena' }
+console.log(stack.size()); // 1
+console.log(stack.peek()); // John Cena
+console.log(stack.push('The Rock')); // { '0': 'John Cena', '1': 'The Rock' }
+console.log(stack.size()); // 2
+stack.print(); // { '0': 'John Cena', '1': 'The Rock' }
+console.log(stack.peek()); // The Rock
+console.log(stack.pop()); // The Rock
+stack.print(); // { '0': 'John Cena' }
+console.log(stack.size()); // 1
+console.log(stack.peek()); // John Cena
+*
+ */
